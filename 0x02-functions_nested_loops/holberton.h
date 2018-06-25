98b1e9ea1505a217d1933525b81456ea432ee27c
Rui Feng <394@holbertonschool.com>
2018-06-25 19:54:31 +0000

    add print_sign

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -40,4 +40,12 @@ int _islower(int c);
  */
 int _isalpha(int c);
 
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to be checked
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is negative
+ */
+int print_sign(int n);
+
 #endif /* _HOLBERTON_H_ */
