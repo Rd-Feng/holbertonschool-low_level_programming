e3ac25dfeb0efdef253706aae2dd5f8a68a96ceb
Rui Feng <394@holbertonschool.com>
2018-06-26 20:04:45 +0000

    first commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even fibonacci numbers starting 1, 2 under 4m
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	unsigned long n, m, tmp;
+	unsigned long sum;
+
+	n = 1;
+	m = 2;
+	sum = n;
+	while (m < 4000000)
+	{
+		if (m % 2 == 0)
+		{
+			sum += m;
+		}
+		tmp = n + m;
+		n = m;
+		m = tmp;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
