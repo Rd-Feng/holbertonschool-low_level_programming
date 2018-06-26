5d6149d15e50339deba5b76fbae2f42a536fa35c
Rui Feng <394@holbertonschool.com>
2018-06-26 15:58:57 +0000

    test push. reset later

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include "6-abs.c"
 /**
  * print_last_digit - Prints the last digit of a number
  * @n: number
@@ -13,7 +13,7 @@ int print_last_digit(int n)
 	tmp = n % 10;
 	if (tmp < 0)
 	{
-		tmp *= -1;
+		tmp = _abs(n);
 	}
 	_putchar('0' + tmp);
 	return (tmp);
