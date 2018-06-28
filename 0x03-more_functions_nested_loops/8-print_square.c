c9219a2bd99676dd7e2914596a992ac06031ae00
Rui Feng <394@holbertonschool.com>
2018-06-28 15:18:42 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_square - print a size x size square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 != 0) && (num % 5 != 0))
+		{
+			printf("%d ", num);
+		}
+		else
+		{
+			if (num % 3 == 0)
+				printf("Fizz");
+			if (num % 5 == 0)
+				printf("Buzz");
+		}
+		if (num == 100)
+			printf("\n");
+		else
+			printf(" ");
+	}
+	return (0);
+}
