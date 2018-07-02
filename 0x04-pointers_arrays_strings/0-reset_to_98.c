5824bb4a9d549c483d1b166573d42a80a235a571
Rui Feng <394@holbertonschool.com>
2018-07-02 17:16:40 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value n points to to 98
+ * @n: pointer that points to the int to be reset
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,19 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - updates the value n points to to 98
+ * @n: pointer that points to the int to be reset
+ */
+void reset_to_98(int *n);
+
+#endif /* _HOLBERTON_H_ */
