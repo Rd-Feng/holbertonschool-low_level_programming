7b09ec4dca01349b6e724d379e20e339cb66738c
Rui Feng <394@holbertonschool.com>
2018-07-07 02:30:58 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+#endif /* _HOLBERTON_H_ */
