2cf11ad7528168031d58a9dfdb892ac61573387f
Rui Feng <394@holbertonschool.com>
2018-07-11 17:54:14 +0000

    first commit

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * helper - guess a factor of n that is not itself or 1
+ * @guess: a guess factor
+ * @n: n
+ *
+ * Return: 1 if no valid factor found, 0 otherwise
+ */
+int helper(int guess, int n)
+{
+	if (guess * guess > n)
+		return (1);
+	else if (n / guess * guess != n)
+		return helper(guess + 1, n);
+	else
+		return (0);
+}
+
+/**
+ * is_prime_number - check if an integer is a prime number
+ * @n: candidate integer
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else
+		return (helper(2, n));
+}
