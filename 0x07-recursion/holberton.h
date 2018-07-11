3b67c11152eb074ff9937b10629501db84325f66
Rui Feng <394@holbertonschool.com>
2018-07-11 16:20:42 +0000

    first commit

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+#endif /* _HOLBERTON_H_ */
