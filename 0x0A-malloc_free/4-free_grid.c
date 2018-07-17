9eed33236abc895199ca692d3d264b8b61e17953
Rui Feng <394@holbertonschool.com>
2018-07-17 19:16:26 +0000

    first commit

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free a grid of memory
+ * @grid: pointer to the grid
+ * @height: height of grid
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid){
+		while (height > 0)
+		{
+			free(*(grid + --height));
+			*(grid + height) = NULL;
+		}
+	}
+	free(grid);
+	grid = NULL;
+}
