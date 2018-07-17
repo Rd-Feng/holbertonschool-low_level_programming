0012658d18b82fe5f64d7c5e11a7d55fbe963e5a
Rui Feng <394@holbertonschool.com>
2018-07-17 18:23:31 +0000

    first commit

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate a 2 dimensional array and initialize to 0
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: pointer to array, NULL on failure or errors
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr = NULL;
+	int i, j,  error = 0;
+
+	if (width > 0 && height > 0)
+	{
+		ptr = (int **) malloc(sizeof(int *) * height);
+		/* allocate memory for array */
+		if (ptr)
+		{
+			for (i = 0; i < height; i++)
+			{
+				*(ptr + i) =
+					(int *) malloc(sizeof(int) * width);
+				if (!(ptr + i))
+				{
+					error = 0;
+					break;
+				}
+			}
+			/* if error occurs, free allocated memory and return */
+			if (error)
+			{
+				while (i >= 0)
+				{
+					free(*(ptr + i));
+					*(ptr + i) = NULL;
+				}
+				free(ptr);
+				ptr = NULL;
+			}
+		}
+		if (ptr)
+		{
+			for (i = 0; i < height; i++)
+				for (j = 0; j < width; j++)
+					ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
