22ce4c67b680ef2334db03983e660b76a10f999c
Rui Feng <394@holbertonschool.com>
2018-07-17 17:02:32 +0000

    first commit

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates and initializes an array of chars
+ * @size: size of array
+ * @c: char to initialize the array with
+ *
+ * Return: 0 on success
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = (char *) malloc(size * sizeof(char));
+	unsigned int i = 0;
+
+	while (i < size)
+		*(ptr + i++) = c;
+	return (ptr);
+}
