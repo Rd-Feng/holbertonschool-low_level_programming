aac3d52c2622b1499924309c61c5a32a9e53b575
Rui Feng <394@holbertonschool.com>
2018-07-17 17:06:01 +0000

    add check if malloc works correctly

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,8 +12,10 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr = (char *) malloc(size * sizeof(char));
 	unsigned int i = 0;
-
-	while (i < size)
-		*(ptr + i++) = c;
+	if (ptr)
+	{
+		while (i < size)
+			*(ptr + i++) = c;
+	}
 	return (ptr);
 }
