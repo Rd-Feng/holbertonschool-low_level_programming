ee1a107c52eb7cf19d87b919a13346acc2b3b474
Rui Feng <394@holbertonschool.com>
2018-07-18 02:57:15 +0000

    new approach

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,39 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-/**
- * str_concat - concatenates two strings and put result in newly
- * allocated space in memory. String 1 will be added a new line char,
- * followed by string 2, followed by a null byte
- * @s1: string 1
- * @s2: string 2
- *
- * Return: pointer to newly allocated space
- */
-char *str_concat(char *s1, char *s2)
-{
-	/* index of terminating null byte, number of chars excluding null */
-	unsigned int len1 = 0, len2 = 0;
-	char *ptr = NULL;
-	unsigned int i;
-
-	if (s1)
-		while (*(s1 + len1))
-			len1++;
-	if (s2)
-		while (*(s2 + len2))
-			len2++;
-	ptr = (char *) malloc(sizeof(char) * (len1 + len2 + 2));
-	if (ptr)
-	{
-		for (i = 0; i < len1; i++)
-			*(ptr + i) = *(s1++);
-		for (; i < len1 + len2; i++)
-			*(ptr + i) = *(s2++);
-		*(ptr + i) = '\n';
-		*(ptr + i + 1) = '\0';
-	}
-	return (ptr);
-}
 
 /**
  * argstostr - concatenates all arguments of this program
@@ -44,30 +10,33 @@ char *str_concat(char *s1, char *s2)
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
-	char *res = NULL, *tmp;
+	char *res = NULL;
+	int len = 0, argLen, i;
 
-	if (ac == 0 || !av)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	/* initialize the res string */
-	if (*av)
-		res = str_concat(*av, "");
+	/* len - total number of chars in arguments, excluding null bytes */
+	for (i = 0; i < ac; i++)
+	{
+		argLen = 0;
+		while (av[i][argLen] != '\0')
+			argLen++;
+		len += argLen;
+	}
+	/* characters of arguments + null byte + 1 new line each arg */
+	res = (char *) malloc(sizeof(char) * (len + ac + 1));
 	if (res)
 	{
-		for (i = 1; i < ac; i++)
+		len = 0;
+		for (i = 0; i < ac; i++)
 		{
-			if (!*(av + i))
+			for (argLen = 0; av[i][argLen] != '\0'; argLen++)
 			{
-				free(res);
-				res = NULL;
-				return (NULL);
+				*(res + len++) = av[i][argLen];
 			}
-			tmp = str_concat(res, *(av + i));
-			free(res);
-			res = tmp;
-			if (!tmp)
-				return (NULL);
+			*(res + len++) = '\n';
 		}
+		*(res + len) = '\0';
 	}
 	return (res);
 }
