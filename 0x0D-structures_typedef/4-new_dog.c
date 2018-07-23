e5f8887aa5c8a91c6896ddc3d7e55371a5cc4857
Rui Feng <394@holbertonschool.com>
2018-07-23 20:32:11 +0000

    first commit

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create new dog using malloc
+ * @name: dog name, string
+ * @age: dog age, float
+ * @owner: owner name, string
+ *
+ * Return: 0 on success
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr = NULL;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr)
+	{
+		ptr->name = name;
+		ptr->age = age;
+		ptr->owner = owner;
+	}
+	return (ptr);
+}
