6eea93ce6fd5db741d2f0c4d75bdf6493f7a6e13
Rui Feng <394@holbertonschool.com>
2018-07-23 20:51:54 +0000

    malloc new memory for string

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 
 /**
+ * _strcpy - make new coppy of string
+ * @str: string
+ *
+ * Return: pointer to new copy of string
+ */
+char *_strcpy(char *str)
+{
+	int len = 0;
+	char *ptr = NULL;
+
+	while (*(str + len))
+		len++;
+	ptr = malloc(sizeof(*ptr) * (len + 1));
+	if (ptr)
+	{
+		while (len >= 0)
+		{
+			*(ptr + len) = *(str + len);
+			len--;
+		}
+	}
+	return (ptr);
+}
+
+/**
  * new_dog - create new dog using malloc
  * @name: dog name, string
  * @age: dog age, float
@@ -21,9 +46,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(ptr);
 			return (NULL);
 		}
-		ptr->name = name;
+		ptr->name = _strcpy(name);
 		ptr->age = age;
-		ptr->owner = owner;
+		ptr->owner = _strcpy(owner);
+		if (!ptr->name || !ptr->owner)
+		{
+			free(ptr);
+			return (NULL);
+		}
 	}
 	return (ptr);
 }
