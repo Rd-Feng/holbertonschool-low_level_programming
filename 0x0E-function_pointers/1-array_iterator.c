97c6dea3e7225ca910dc21e3328ed7322ca768c9
Rui Feng <394@holbertonschool.com>
2018-07-25 17:11:43 +0000

    first commit

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on element of an array
+ * @array: array
+ * @size: number of elements
+ * @action: function to execute on elements
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && size > (unsigned int) 0 && action)
+		for (i = 0; (unsigned int) i < size; i++)
+			action(*(array + i));
+}
