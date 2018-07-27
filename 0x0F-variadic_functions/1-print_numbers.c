6735ead8432b7e30602f87958bc472c8ba2ed931
Rui Feng <394@holbertonschool.com>
2018-07-27 15:47:55 +0000

    first commit

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print passed in numbers separated by separator string
+ * @separator: separator string
+ * @n: number of parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list params;
+	unsigned i;
+
+	va_start(params, n);
+	for (i = 1; i <= n; i++)
+	{
+		if (i == n)
+			printf("%d\n", va_arg(params, int));
+		else
+			printf("%d%s", va_arg(params, int),
+			       separator ? separator : "");
+	}
+	va_end(params);
+}
