369e778ef661b773aa6fdebbb75671e85caccb0b
Rui Feng <394@holbertonschool.com>
2018-07-28 20:50:47 +0000

    put null byte check in null check for format

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -22,41 +22,37 @@ void _puts(char *str)
  */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i = 0;
 	va_list params;
 
 	va_start(params, format);
-	if (format)
+	while (format && *(format + i))
 	{
-		i = 0;
-		while (*(format + i))
+		switch (*(format + i))
 		{
-			switch (*(format + i))
-			{
-			case 'c':
-				printf("%c", va_arg(params, int));
+		case 'c':
+			printf("%c", va_arg(params, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(params, int));
 				break;
-			case 'i':
-				printf("%d", va_arg(params, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(params, double));
-				break;
-			case 's':
-				_puts(va_arg(params, char*));
-				break;
-			default:
-				i++;
-				continue;
-			}
-			if (!*(format + i + 1))
-			{
-				printf("\n");
-				break;
-			}
-			printf(", ");
+		case 'f':
+			printf("%f", va_arg(params, double));
+			break;
+		case 's':
+			_puts(va_arg(params, char*));
+			break;
+		default:
 			i++;
+			continue;
 		}
+		if (!*(format + i + 1))
+		{
+			printf("\n");
+			break;
+		}
+		printf(", ");
+		i++;
 	}
 	va_end(params);
 }
