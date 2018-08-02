328a5120fbe5172d74b9f3ee42a2817ee47f1fed
Rui Feng <394@holbertonschool.com>
2018-08-02 16:16:33 +0000

    first commit

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - insert node at the beginning of a list
+ * @head: pointer to pointer of first element in list
+ * @str: string in new inserted node
+ *
+ * Return: 0 on success
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode = NULL;
+	char *newStr = NULL;
+	unsigned int i = 0;
+
+	if (str)
+		newStr = strdup(str);
+	if (head && newStr)
+	{
+		newNode = malloc(sizeof(list_t));
+		if (newNode)
+		{
+			newNode->str = newStr;
+			while (newStr[i])
+				i++;
+			newNode->len = i;
+			newNode->next = *head ? *head : NULL;
+			*head = newNode;
+		}
+	}
+	return (newNode);
+}
