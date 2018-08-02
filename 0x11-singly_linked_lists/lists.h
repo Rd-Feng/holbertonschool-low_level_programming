ad7f1cac2b4edc558da1f9d396552903c7003b27
Rui Feng <394@holbertonschool.com>
2018-08-02 14:28:34 +0000

    initial commit

---
+++ 0x11-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+
+#endif
