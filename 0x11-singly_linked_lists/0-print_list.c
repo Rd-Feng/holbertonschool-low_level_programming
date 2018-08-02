7e7beaf5af79bc59a54778eac32ff2033c695d7b
Rui Feng <394@holbertonschool.com>
2018-08-02 15:46:11 +0000

    remove _strlen function

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-
-unsigned int _strlen(char *s);
-
 /**
  * print_list - prints all elements of a list
  * @h: first node in list
@@ -12,29 +9,16 @@ unsigned int _strlen(char *s);
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+	unsigned int i;
 
 	while (h)
 	{
-		printf("[%u] %s\n", _strlen(h->str),
-		       (h->str) ? h->str : "(nil)");
+		i = 0;
+		while (h->str && h->str[i])
+			i++;
+		printf("[%u] %s\n", i, (h->str) ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
 	return (count);
-}
-
-/**
- * _strlen - get number of characters in a string excluding null byte
- * @s: string
- *
- * Return: lenth of string, 0 if string is empty or null
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int i = 0;
-
-	if (s)
-		while (s[i])
-			i++;
-	return (i);
 }
--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct list_s - singly linked list
@@ -23,4 +23,6 @@ size_t print_list(const list_t *h);
 
 size_t list_len(const list_t *h);
 
-#endif
+list_t *add_node(list_t **head, const char *str);
+
+#endif /* LISTS_H */
