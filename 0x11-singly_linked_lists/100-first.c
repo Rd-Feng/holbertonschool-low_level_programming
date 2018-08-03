11ee331198062a6c8f87f6da30046931d4c6130c
Rui Feng <394@holbertonschool.com>
2018-08-03 01:18:36 +0000

    betty

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+__attribute__ ((__constructor__))
 /**
  * premain - function execute before main
  */
-__attribute__ ((__constructor__))
 void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
