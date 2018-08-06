cf05103e908ecb17fcb98746d6646e9f773db2c8
Rui Feng <394@holbertonschool.com>
2018-08-06 15:38:00 +0000

    first commit

---
+++ 0x12-more_singly_linked_lists/dummy.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 on success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	return (0);
+}
