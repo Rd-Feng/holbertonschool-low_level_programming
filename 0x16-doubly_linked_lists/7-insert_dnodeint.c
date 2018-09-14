1eecefa7dc8e14c012dd88dc6e911241f0455bfd
Rui Feng <394@holbertonschool.com>
2018-09-14 22:38:54 +0000

    reduce number of lines

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,25 +11,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *ptr = NULL, *newNode = NULL;
 
-	if (h)
-	{
-		if (idx == 0)
-			return (add_dnodeint(h, n));
-		for (ptr = *h; idx > 1 && ptr; idx--, ptr = ptr->next)
-			;
-		if (ptr)
-		{
-			newNode = malloc(sizeof(dlistint_t));
-			if (newNode)
-			{
-				newNode->n = n;
-				newNode->next = ptr->next;
-				newNode->prev = ptr;
-				if (ptr->next)
-					ptr->next->prev = newNode;
-				ptr->next = newNode;
-			}
-		}
-	}
+	if (!h)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	for (ptr = *h; idx > 1 && ptr; idx--, ptr = ptr->next)
+		;
+	if (!ptr)
+		return (NULL);
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = ptr->next;
+	newNode->prev = ptr;
+	if (ptr->next)
+		ptr->next->prev = newNode;
+	ptr->next = newNode;
 	return (newNode);
 }
