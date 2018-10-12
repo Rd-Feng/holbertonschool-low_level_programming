dcbca04996da1de9059d3bd9d908c5b4815e44fa
Rui Feng <394@holbertonschool.com>
2018-10-12 18:58:38 +0000

    over 40 lines

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,6 +13,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *newNode = NULL, *ptr = NULL;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	if (!ht || !key || *key == 0)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
