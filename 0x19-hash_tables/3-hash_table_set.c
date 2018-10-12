46e338dde46df348f91072cc93d3fbd439753d32
Rui Feng <394@holbertonschool.com>
2018-10-12 19:00:12 +0000

    revert

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,24 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *newNode = NULL, *ptr = NULL;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	if (!ht || !key || *key == 0)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
