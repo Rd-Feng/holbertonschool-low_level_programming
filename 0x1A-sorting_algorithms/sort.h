c66739089a29a03f87e58ec4921c22295bff5225
Rui Feng <394@holbertonschool.com>
2018-10-24 19:45:33 +0000

    protect double include

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -19,3 +22,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+#endif
