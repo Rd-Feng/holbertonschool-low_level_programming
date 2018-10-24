ca8b2c8078a3cb313b88fa40a629e73037262b72
Rui Feng <394@holbertonschool.com>
2018-10-24 21:36:19 +0000

    init

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+void swap(int *array, int i, int j, size_t size);
+/**
+ * quick_sort - quick sort array
+ * @array: array
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	int p_idx, chk, tmp;
+
+	if (size < 2)
+		return;
+}
