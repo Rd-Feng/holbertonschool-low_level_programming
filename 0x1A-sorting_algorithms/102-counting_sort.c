020865ac21a481c545f79a9901ebfc45c9b581f5
sumin3 <suminyu@yahoo.com>
2018-10-25 21:41:22 +0000

    add helper

--- 0x1A-sorting_algorithms/102-counting_sort.c
+++ 0x1A-sorting_algorithms/102-counting_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 int _max(int *array, size_t size);
-int *count_array(int *array, size_t size, int k);
+int *setup_array(int *array, size_t size, int k);
 
 void counting_sort(int *array, size_t size)
 {
@@ -16,16 +16,21 @@ void counting_sort(int *array, size_t size)
 
 }
 
-int *count_array(int *array, size_t size, int k)
+int *setup_array(int *array, size_t size, int k)
 {
 	int *countArray = NULL;
+	int i;
 
 	countArray = malloc(sizeof(int) * (k + 1));
 	if (!countArray)
 		return (NULL);
-	
-
-	
+	for (i = 0; i < k + 1; i++)
+		countArray[i] = 0;
+	for (i = 0; i < size; i++)
+		countArray[array[i]] += 1;
+	for (i = 0; i < k; i++)
+		countArray[i + 1] = countArray[i] + countArray[i + 1];
+	print_array(countArray, k + 1)
 }
 /**
  * _max - find the largest value of an array
