e4b4e433e19d3a149852d4de986a3a77b08082d1
Rui Feng <394@holbertonschool.com>
2018-10-26 21:58:19 +0000

    size_t

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
-void swap(int *array, int i, int j, size_t size);
-void helper(int *array, size_t size, int lo, int hi);
-int partition(int *array, size_t size, int lo, int hi);
+void swap(int *array, size_t i, size_t j, size_t size);
+void helper(int *array, size_t size, size_t lo, size_t hi);
+size_t partition(int *array, size_t size, size_t lo, size_t hi);
 /**
  * quick_sort - quick sort array
  * @array: array
@@ -18,14 +18,15 @@ void quick_sort(int *array, size_t size)
  * @lo: lower bound
  * @hi: upper bound
  */
-void helper(int *array, size_t size, int lo, int hi)
+void helper(int *array, size_t size, size_t lo, size_t hi)
 {
-	int p;
+	size_t p;
 
 	if (lo < hi)
 	{
 		p = partition(array, size, lo, hi);
-		helper(array, size, lo, p - 1);
+		if (p > 0)
+			helper(array, size, lo, p - 1);
 		helper(array, size, p + 1, hi);
 	}
 }
@@ -38,19 +39,20 @@ void helper(int *array, size_t size, int lo, int hi)
  *
  * Return: pivot index
  */
-int partition(int *array, size_t size, int lo, int hi)
+size_t partition(int *array, size_t size, size_t lo, size_t hi)
 {
-	int i, pivot, j;
+	int pivot;
+	size_t i, j;
 
-	for (i = lo - 1, j = lo, pivot = array[hi]; j < hi; j++)
+	for (i = lo, j = lo, pivot = array[hi]; j < hi; j++)
 	{
 		if (array[j] < pivot && i++)
-			if (i != j)
-				swap(array, i, j, size);
+			if (i - 1 != j)
+				swap(array, i - 1, j, size);
 	}
-	if (i + 1 != hi)
-		swap(array, i + 1, hi, size);
-	return (i + 1);
+	if (i != hi)
+		swap(array, i, hi, size);
+	return (i);
 }
 /**
  * swap - swap two elements and print array
@@ -59,7 +61,7 @@ int partition(int *array, size_t size, int lo, int hi)
  * @j: j
  * @size: size
  */
-void swap(int *array, int i, int j, size_t size)
+void swap(int *array, size_t i, size_t j, size_t size)
 {
 	int tmp;
 
