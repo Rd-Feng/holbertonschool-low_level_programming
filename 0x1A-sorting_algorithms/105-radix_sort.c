f91d1a329eaf4ff22f6dbf1dcb201cf9ee10dfd7
sumin3 <suminyu@yahoo.com>
2018-10-30 23:16:35 +0000

    save

--- 0x1A-sorting_algorithms/105-radix_sort.c
+++ 0x1A-sorting_algorithms/105-radix_sort.c
@@ -19,8 +19,12 @@ void radix_sort(int *array, size_t size)
 			max_num = array[i];
 	for (n = max_num, max_ex = 0; n > 10; max_ex++)
 		n /= 10;
-	for (n = 0; n <= max_ex; n++)
+	for (n = 0; n <= max_ex; n++, print_array(array, size))
+	{
+		printf("b4 count sort\n");
 		_counting_sort(array, size, n);
+		printf("af count sort\n");
+	}
 }
 /**
  * _counting_sort - counting sort on array
@@ -31,19 +35,25 @@ void radix_sort(int *array, size_t size)
 void _counting_sort(int *array, size_t size, int ex)
 {
 	int *count_array = NULL, *tmp = NULL;
-	size_t i, k;
+	size_t i, k, factor;
 
-	if (!array || size < 2)
-		return;
 	tmp = malloc(sizeof(int) * size);
 	if (!tmp)
 		return;
 	for (i = 0; i < size; i++)
 		tmp[i] = array[i];
+	printf("b4 max ex%d\n ", ex);
 	k = _max(array, size, ex);
+	printf("af max\n");
+	printf("b4 countarrray\n");
 	count_array = setup_count_array(array, size, k, ex);
+	printf("af countarrray\n");
+	for (factor = 1; ex > 0; factor *= 10, ex--)
+		;
 	for (i = 0; i < size; i++)
-		array[--count_array[tmp[i]]] = tmp[i];
+	{
+		array[--count_array[tmp[i] / factor % 10]] = tmp[i];
+	}
 	free(tmp);
 	free(count_array);
 }
@@ -62,8 +72,9 @@ int *setup_count_array(int *array, size_t size, size_t k, int ex)
 	int d = 0, factor = 1, j;
 
 	countArray = malloc(sizeof(int) * (k + 1));
-	for (j = 0; j <= ex; factor *= 10)
+	for (j = 0;  j < ex; factor *= 10, j++)
 		;
+	printf("factor %d\n", factor);
 	if (!countArray)
 		return (NULL);
 	for (i = 0; i < k + 1; i++)
@@ -71,9 +82,16 @@ int *setup_count_array(int *array, size_t size, size_t k, int ex)
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] >= factor)
+		{
 			d = array[i]/ factor % 10;
+			printf("d = %d array[%lu] = %d factor %d\n", d, i, array[i], factor);
+		}
+		printf("d = %d\n", d);
 		countArray[d] += 1;
 	}
+	printf("-----count array in setup ----\n");
+	print_array(countArray, size);
+	printf("-----count array in setup ----end\n");
 	for (i = 0; i < k; i++)
 		countArray[i + 1] = countArray[i] + countArray[i + 1];
 	return (countArray);
@@ -87,10 +105,9 @@ int *setup_count_array(int *array, size_t size, size_t k, int ex)
  */
 size_t _max(int *array, size_t size, int ex)
 {
-	size_t i;
-	int max, factor;
+	size_t i, max, factor;
 
-	for (factor = 1; ex > 0; factor *= 10)
+	for (factor = 1; ex > 0; factor *= 10, ex--)
 		;
 	max = array[0] / factor % 10;
 	for (i = 0; i < size; i++)
