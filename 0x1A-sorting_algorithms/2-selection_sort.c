8107af7227134f05ccf6d9913a796902cc1ec07a
sumin3 <suminyu@yahoo.com>
2018-11-02 02:27:52 +0000

    refactor

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -8,22 +8,18 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, tmp, min_idx;
-	int cur_min;
 
 	if (!array)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		cur_min = array[i];
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (cur_min > array[j])
-			{
-				cur_min = array[j];
+			if (array[min_idx] > array[j])
 				min_idx = j;
-			}
 		}
-		if (cur_min != array[i])
+		if (min_idx != i)
 		{
 			tmp = array[i];
 			array[i] = array[min_idx];
