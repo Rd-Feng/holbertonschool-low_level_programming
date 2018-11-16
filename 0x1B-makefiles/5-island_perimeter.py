ac97f56a5b7380a0ae47500e4ec2b3c8dca09b84
Rui Feng <394@holbertonschool.com>
2018-11-16 20:03:41 +0000

    init commit

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""island_perimeter module
+defines function island_perimeter"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island"""
+    up, left, right, down = 0, 0, len(grid[0]), len(grid)
+    p = 0
+    for i in range(up + 1, down - 1):
+        for j in range(left + 1, right - 1):
+            if grid[i][j] == 1:
+                # check up
+                p += 1 if not grid[i - 1][j] else 0
+                # check left
+                p += 1 if not grid[i][j - 1] else 0
+                # check right
+                p += 1 if not grid[i][j + 1] else 0
+                # check down
+                p += 1 if not grid[i + 1][j] else 0
+    return p
