089e99116af4d8b1d2c7a30016e423025d8e3302
Rui Feng <394@holbertonschool.com>
2018-11-16 20:48:56 +0000

    check edge of grid

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -7,11 +7,11 @@ def island_perimeter(grid):
     """returns the perimeter of the island"""
     up, left, right, down = 0, 0, len(grid[0]), len(grid)
     p = 0
-    for i in range(up + 1, down - 1):
-        for j in range(left + 1, right - 1):
+    for i in range(up, down):
+        for j in range(left, right):
             if grid[i][j] == 1:
-                p += 1 if not grid[i - 1][j] else 0
-                p += 1 if not grid[i][j - 1] else 0
-                p += 1 if not grid[i][j + 1] else 0
-                p += 1 if not grid[i + 1][j] else 0
+                p += 1 if i == up or not grid[i - 1][j] else 0
+                p += 1 if j == left - 1 or not grid[i][j - 1] else 0
+                p += 1 if j == right - 1 or not grid[i][j + 1] else 0
+                p += 1 if i == down - 1 or not grid[i + 1][j] else 0
     return p
