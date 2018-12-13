3ae5e3006968e0d501841714095c19e1a0982c23
Rui Feng <394@holbertonschool.com>
2018-12-13 15:30:28 +0000

    keep track of previous node using double pointer

--- 0x1C-binary_trees/110-binary_tree_is_bst.c
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 #include <stdio.h>
-int bst_inorder_check(binary_tree_t *tree, int *prev);
+int bst_inorder_check(binary_tree_t *tree, binary_tree_t **prev);
 /**
  * binary_tree_is_bst - check if a tree is bst
  * @tree: tree to check
@@ -9,38 +9,30 @@ int bst_inorder_check(binary_tree_t *tree, int *prev);
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int *prev = NULL, ret;
+	binary_tree_t **prev = NULL;
 
-	if (!tree)
-		return (0);
-	ret = bst_inorder_check((binary_tree_t *) tree, prev);
-	if (prev)
-		free(prev);
-	return (ret);
+	if (tree)
+		return (bst_inorder_check((binary_tree_t *) tree, prev));
+	return (0);
 }
 /**
  * bst_inorder_check - in order traverse a bst. check if sorted
  * @tree: tree
- * @prev: data in previous node
+ * @prev: pointer to previous node
  *
  * Return: 1 if sorted traverse, 0 otherwise
  */
-int bst_inorder_check(binary_tree_t *tree, int *prev)
+int bst_inorder_check(binary_tree_t *tree, binary_tree_t **prev)
 {
 	if (tree)
 	{
-		if (bst_inorder_check(tree->left, prev))
-		{
-			if (prev && tree->n < *prev)
-				return (0);
-			if (!prev)
-				prev = malloc(sizeof(int));
-			*prev = tree->n;
-			if (!bst_inorder_check(tree->right, prev))
-				return (0);
-			return (1);
-		}
-		return (0);
+		if (!bst_inorder_check(tree->left, prev))
+			return (0);
+		if (prev && (*prev)->n > tree->n)
+			return (0);
+		prev = &tree;
+		if (!bst_inorder_check(tree->right, prev))
+			return (0);
 	}
 	return (1);
 }
